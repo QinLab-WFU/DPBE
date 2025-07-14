38dafd6f3574cf3c362eea8f9462b90c2972344c
QinLab-WFU <128210944+QinLab-WFU@users.noreply.github.com>
2025-07-14 17:27:51 +0800

    Update get_args.py

--- train/DPBE/get_args.py
+++ train/DPBE/get_args.py
@@ -1,24 +1,28 @@
+import argparse
 import os
 
 from argsbase import get_baseargs
 
 
-def get_args():
+def get_args(main_args):
 
     parser = get_baseargs()
 
     parser.add_argument('--use-lam', default=True, help='Use lam')
     parser.add_argument('--loss', default="acm")
-    parser.add_argument("--miner", default="dwm", help="choice form [dwm, semi, random]")
-    parser.add_argument('--train-n-samples', default=1)
-    parser.add_argument('--valid-n-samples', default=1)
+    parser.add_argument('--train-n-samples', default=5)
+    parser.add_argument('--valid-n-samples', default=5)
     parser.add_argument('--max-pairs', default=5000)
     parser.add_argument('--hessian-memory-factor', default=0.999, type=float, help='Dropout rate')
 
     parser.add_argument('--calculate-uncertainty', default=False)
 
+    parser.add_argument('-warm-up', default=20)
+    parser.add_argument('--noise-rate', default=0)
+
     args = parser.parse_args()
-    args.method = 'DPBE'
-    args.save_dir = os.path.join(args.save_dir, args.method, str(args.output_dim))
+    args = argparse.Namespace(**vars(args), **vars(main_args))
+
+    args.save_dir = os.path.join(args.save_dir, args.method, args.dataset, str(args.output_dim), str(args.noise_rate))
 
     return args
