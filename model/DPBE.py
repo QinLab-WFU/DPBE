91d8a33d64d21838ee781992e09863e6103b3900
QinLab-WFU <128210944+QinLab-WFU@users.noreply.github.com>
2025-07-14 17:22:36 +0800

    Update DPBE.py

--- model/DPBE.py
+++ model/DPBE.py
@@ -12,7 +12,6 @@ class MDPBE(Baseclip):
     def __init__(self,
                  use_lam=True,
                  outputDim=64,
-                 num_classes=80,
                  clipPath="./ViT-B-32.pt",
                  writer=None,
                  saveDir="./result/log",
@@ -21,16 +20,24 @@ class MDPBE(Baseclip):
         super(MDPBE, self).__init__(outputDim=outputDim, clipPath=clipPath, writer=writer,
                                     saveDir=saveDir, logger=logger, is_train=is_train)
 
-        self.image_pre = Pre_Layer(inputdim=self.embedDim, nb_class=num_classes)
-        self.text_pre = Pre_Layer(inputdim=self.embedDim, nb_class=num_classes)
-
         if use_lam:
             self.image_hash = nn.Sequential(nn.Linear(self.embedDim, outputDim), L2Norm())
             self.text_hash = nn.Sequential(nn.Linear(self.embedDim, outputDim), L2Norm())
 
-    def forward(self, image, text):
+        def encoding(self, image, text):
         image_embed = self.clip.encode_image(image)
         text_embed = self.clip.encode_text(text)
-        image_pre = self.image_pre(image_embed)
-        text_pre = self.text_pre(text_embed)
-        return image_embed, text_embed, image_pre, text_pre
+        return image_embed, text_embed
+
+    def hashing(self, image_embed, text_embed):
+        image_hash = self.image_hash(image_embed)
+        text_hash = self.text_hash(text_embed)
+        return image_hash, text_hash
+
+    def forward(self, image, text):
+
+        image_embed = self.clip.encode_image(image)
+        text_embed = self.clip.encode_text(text)
+        image_embed = self.image_hash(image_embed)
+        text_embed = self.text_hash(text_embed)
+        return image_embed, text_embed
